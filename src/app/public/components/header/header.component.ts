bb27736b855364828a96eb5647477ea2b93e3605
Nadia <nad21lc@gmail.com>
2024-05-02 20:18:22 -0500

    first commit

---
+++ src/app/public/components/header/header.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from "@angular/material/button";
+
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [MatToolbarModule, MatIconModule, MatButtonModule],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css'
+})
+export class HeaderComponent {
+
+    constructor(private router: Router) { }
+
+    redirectToMovies() {
+        this.router.navigate(['bussines/peliculas']);
+    }
+
+    redirectToHome() {
+        this.router.navigate(['home']);
+    }
+}
